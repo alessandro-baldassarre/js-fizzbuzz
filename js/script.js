c0e3b8a635915cf0a895f4c34f4b3e50a9f57a75
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-02-24 17:41:01 +0100

    added html/css interface (DOM manipulation)

--- js/script.js
+++ js/script.js
@@ -6,19 +6,39 @@
 
 
 for (let i = 1; i <= 100; i++){
+
+    const wrapperBox = document.querySelector("#wrapper-box");
+    let newBox = document.createElement("div");
     
+
+    newBox.classList.add("box", "border", "border-dark", "m-5", "rounded-circle", "text-center");
+    newBox.innerHTML = i;
+    wrapperBox.append(newBox);
+    
+    
+
     if((i % 3) == 0){
         if((i % 5) == 0){
+            
             console.log(i + " FizzBuzz");
+            newBox.classList.remove("border-dark");
+            newBox.classList.add("border-success", "border-5");
+            
         }
         else{
             console.log(i + " Fizz");
+            newBox.classList.remove("border-dark");
+            newBox.classList.add("border-danger", "border-5");
         }
     }
     else if((i % 5) == 0){
         console.log(i + " Buzz");
+        newBox.classList.remove("border-dark");
+            newBox.classList.add("border-warning", "border-5");
     }
     else{
         console.log(i);
     }
+
 }
+
